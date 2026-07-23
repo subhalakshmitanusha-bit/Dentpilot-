7e0eb52dec760dc516b59c8cd78c1d5ae7d19831
subhalakshmitanusha-bit <subhalakshmitanusha@gmail.com>
2026-07-23 09:52:48 +0530

    Initialize Flask app with home and health routes

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return jsonify({
+        "app": "DentPilot",
+        "status": "running",
+        "message": "DentPilot backend is working."
+    })
+
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "healthy"
+    })
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
